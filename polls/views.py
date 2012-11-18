1c3b7372f57d722993edd7207779098bca60bb3d
awt2gbg2012 <awt2.2012@gmail.com>
2012-11-18 23:36:31 +0100

    Tutorial 4 halfway through

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from polls.models import Poll
-from django.http import HttpResponse
+from polls.models import Poll, Choice
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 
 def index(request):
@@ -10,12 +12,28 @@ def index(request):
 
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
+    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 
 
 def results(request, poll_id):
-    return HttpResponse('Poll result %s' % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
 
 
 def vote(request, poll_id):
-    return HttpResponse('Poll vote %s' % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the voting form
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You did not select a choice",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return a HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
